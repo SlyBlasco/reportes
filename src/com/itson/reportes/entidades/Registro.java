889d5f7f43cfc74b7662c054b1c24f7ad8138c43
Luis Blasco <luism@192.168.1.79>
2024-05-12 19:58:29 -0700

    Se crearon y implementaron las entidades Carro y Registro

---
+++ src/com/itson/reportes/entidades/Registro.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.itson.reportes.entidades;
+
+import java.sql.Date;
+/**
+ *
+ * @author Luis Blasco, Daniel Labrada, Emiliano Bojorquez, Miguel Devora, Mario Le Blohic.
+ */
+public class Registro {
+    private int id;
+    private Date fecha;
+    private String estado;
+    private boolean pagado;
+    private int idCarro;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public boolean isPagado() {
+        return pagado;
+    }
+
+    public void setPagado(boolean pagado) {
+        this.pagado = pagado;
+    }
+
+    public int getIdCarro() {
+        return idCarro;
+    }
+
+    public void setIdCarro(int idCarro) {
+        this.idCarro = idCarro;
+    }
+    
+    
+}
